112d6ccd6e42ff0eee15926199cceefb8f9969b9
a.a.amouzgar <amiraliamouzgar@gmail.com>
2026-07-09 19:29:32 +0330

    feat(frontend): introduce navigation manager

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.secondhand.frontend {
     requires java.net.http;
 
     opens com.secondhand.frontend to javafx.fxml;
+    opens com.secondhand.frontend.controller.auth to javafx.fxml;
     exports com.secondhand.frontend;
     exports com.secondhand.frontend.controller;
     opens com.secondhand.frontend.controller to javafx.fxml;
