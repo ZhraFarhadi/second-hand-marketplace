25c81db513720bb86d0860e1238c36f81fb03812
a.a.amouzgar <amiraliamouzgar@gmail.com>
2026-07-21 03:30:05 +0330

    refactor(frontend): add shared utility classes

--- frontend/src/main/java/com/secondhand/frontend/util/JsonUtil.java
+++ frontend/src/main/java/com/secondhand/frontend/util/JsonUtil.java
@@ -1,14 +1,27 @@
 package com.secondhand.frontend.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
 
 public final class JsonUtil {
 
-    private static final Gson GSON =
-            new GsonBuilder().create();
-
     private JsonUtil() {}
+
+    private static final Gson GSON =
+            new GsonBuilder()
+                    .registerTypeAdapter(LocalDateTime.class,
+                            new JsonDeserializer<LocalDateTime>() {
+                                @Override
+                                public LocalDateTime deserialize(
+                                        JsonElement json,
+                                        Type typeOfT,
+                                        JsonDeserializationContext context
+                                ) {
+                                    return LocalDateTime.parse(json.getAsString());
+                                }
+                            })
+                    .create();
 
     public static Gson getGson() {
         return GSON;
