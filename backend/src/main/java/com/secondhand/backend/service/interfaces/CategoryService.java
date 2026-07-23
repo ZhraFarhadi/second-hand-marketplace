070c3d19ed172a2521290ad6cd50c678c2fe2a3b
Zahra <zhra.farhadii@gmail.com>
2026-07-23 22:12:37 +0330

    nothing

--- backend/src/main/java/com/secondhand/backend/service/interfaces/CategoryService.java
+++ backend/src/main/java/com/secondhand/backend/service/interfaces/CategoryService.java
@@ -2,10 +2,7 @@ package com.secondhand.backend.service.interfaces;
 
 import com.secondhand.backend.dto.category.request.CreateCategoryRequest;
 import com.secondhand.backend.dto.category.request.UpdateCategoryRequest;
-import com.secondhand.backend.dto.category.response.CategoryBreadcrumbItemResponse;
-import com.secondhand.backend.dto.category.response.CategoryChildrenResponse;
-import com.secondhand.backend.dto.category.response.CategorySummaryResponse;
-import com.secondhand.backend.dto.category.response.CategoryWithAttributesResponse;
+import com.secondhand.backend.dto.category.response.*;
 
 import java.util.List;
 
